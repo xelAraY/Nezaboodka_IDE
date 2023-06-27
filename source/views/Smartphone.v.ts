5d524b5fe05d42f33355081d07d36545f9b19e70
filka55533 <ifilipovich.if@gmail.com>
2023-06-27 22:41:24 +0000

    Refactor smartphone view

--- source/views/Smartphone.v.ts
+++ source/views/Smartphone.v.ts
@@ -5,34 +5,26 @@ import { WorkArea } from "./WorkArea.v";
 
 export const Smartphone = (theme: AppTheme, body?: BlockBody<HTMLElement, void, void>) => (
   Block(body, {reaction: true,
-    initialize(b) {
-
-    },
     render(b, base) {
+      base();
+      b.style(theme.RightPanel);
+      b.style(theme.accentColor);
+      b.widthGrowth = 3;
+      b.heightGrowth = 1;
 
-      base()
-      b.style(theme.RightPanel)
-      b.style(theme.accentColor)
-      b.widthGrowth = 3
-      b.heightGrowth = 1
-
-      b.contentAlignment = Align.Center | Align.CenterV
+      b.contentAlignment = Align.Center | Align.CenterV;
 
       Block({initialize(b){
         b.style(css`
           height: 630px;
           width: 430px;
-        `)
+        `);
       },
       render(){
-
         WorkArea({render(b, base) {
-          base()
-        }})
-
-      }})
-
+          base();
+        }});
+      }});
     }
   })
-
 )
